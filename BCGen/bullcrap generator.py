6f8f43297712cf475a690f6b8ad142906ba5841e
Kpaiy <craigjarrodonly@gmail.com>
2014-11-13 20:21:03 +1100

    BCGen

--- BCGen/bullcrap generator.py
+++ BCGen/bullcrap generator.py
@@ -8,7 +8,7 @@ To do this, I had to state the amount of passages and use an if statement
 in the getSentence function.
 '''
 
-#0 = imperative, 1 = present, 2 = continuous, 3 = perfect
+#0 = imperative, 1 = present, 2 = continuous, 3 = perfect, 4 = perfect passive participle
 verbs_file = open('verbs.db')
 nouns_file = open('nouns.db')
 adjectives_file = open('adjectives.db')
@@ -38,7 +38,7 @@ nouns_file.close()
 adjectives_file.close()
 adverbs_file.close()
 #These close the files, because they are not needed anymore.
-passages = 4 #Set to number of passages in the getSentence function.
+passages = 7 #Set to number of passages in the getSentence function.
 
 '''
 These get functions will return a random word.
@@ -46,7 +46,7 @@ These get functions will return a random word.
 In the case of verbs, a tense argument is required to give tense.
 For a random tense, just use getVerb(random.randrange(3))
 0 is imperative or base word, 1 is present for singular noun,
-2 is continuous, and 3 is past perfect.
+2 is continuous, 3 is past perfect, and 4 is perfect passive participle.
 
 For nouns, an argument of number is needed.
 0 for singular, 1 for plural. For a random number, just use
@@ -70,11 +70,17 @@ def getSentence():
 	if picker == 0:
 		out = ("The " + getNoun(random.randrange(1)) + " " + getAdverb() + " " + getVerb(3) + " the " + getNoun(random.randrange(1)) + ".")
 	if picker == 1:
-		out = ("The " + getAdjective() + " " + getNoun(1) + " were " + getVerb(3) + " by the " + getAdjective() + " " + getNoun(random.randrange(1)) + ".")
+		out = ("The " + getAdjective() + " " + getNoun(1) + " were " + getVerb(4) + " by the " + getAdjective() + " " + getNoun(random.randrange(1)) + ".")
 	if picker == 2:
 		out = ("Why, oh why, does the " + getAdjective() + " " + getNoun(0) + " " + getVerb(0) + "?")
 	if picker == 3:
-		out = ("Your " + getAdjective() + " " + getNoun(random.randrange(1)) + " is as " + getAdjective() + " as the " + getAdjective() + " " + getNoun(random.randrange(1)) + ".") 
+		out = ("Your " + getAdjective() + " " + getNoun(random.randrange(1)) + " is as " + getAdjective() + " as the " + getAdjective() + " " + getNoun(random.randrange(1)) + ".")
+	if picker == 4:
+		out = ("Stop " + getAdverb() + " " + getVerb(2) + " the " + getAdjective() + " " + getNoun(0) + "!")
+	if picker == 5:
+		out = ("Why must you " + getVerb(0) + " the " + getNoun(1) + "?")
+	if picker == 6:
+		out = ("You're just like the " + getAdjective() + " " + getNoun(0) + ", in that you both " + getAdverb() + " " + getVerb(0) + ".")
 	return(out)
 
 #print("The",getNoun(random.randrange(1)),getAdverb(),getVerb(3),"the",getNoun(random.randrange(1)) + ".")
